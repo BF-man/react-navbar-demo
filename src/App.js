67c360b3529c7b2532aa082542849f0623aebb2a
BF-man <bf-man@mail.ru>
2018-10-06 17:55:38 +0300

    Remove trash

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
-import './App.css';
-
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar />
         <Search />
       </div>
